4b7231a28a66053bbc444b48ac5a03fb69cc6b64
lanou <lanou@ZCL.local>
2017-12-05 18:30:43 +0800

    Wine实体类
    Wine实体类添加属性

--- src/main/java/com/lanou/entity/Wine.java
+++ src/main/java/com/lanou/entity/Wine.java
@@ -7,6 +7,8 @@ public class Wine {
     private Integer WId;
     private String WineName;
     private String WineLiter;
+    private String WineEvalution;
+    private Integer WineEvaCount;
     private double WinePrice;
     private String WineCompany;
     private String WineSales;
@@ -15,7 +17,11 @@ public class Wine {
     private String WineDegree;
     private String WineAdress;
     private String WineShiyong;
+    private String WineCity;
+    private String FromAdress;
+    private String WineBianhao;
     private Integer WineCount;
+    private String WineContain;
     private String WineImg1;
     private String WineImg2;
     private String WineImg3;
@@ -26,6 +32,37 @@ public class Wine {
 
     public Wine() {
         super();
+    }
+
+    @Override
+    public String toString() {
+        return "Wine{" +
+                "WId=" + WId +
+                ", WineName='" + WineName + '\'' +
+                ", WineLiter='" + WineLiter + '\'' +
+                ", WineEvalution='" + WineEvalution + '\'' +
+                ", WineEvaCount=" + WineEvaCount +
+                ", WinePrice=" + WinePrice +
+                ", WineCompany='" + WineCompany + '\'' +
+                ", WineSales='" + WineSales + '\'' +
+                ", WineScent='" + WineScent + '\'' +
+                ", WineBrand='" + WineBrand + '\'' +
+                ", WineDegree='" + WineDegree + '\'' +
+                ", WineAdress='" + WineAdress + '\'' +
+                ", WineShiyong='" + WineShiyong + '\'' +
+                ", WineCity='" + WineCity + '\'' +
+                ", FromAdress='" + FromAdress + '\'' +
+                ", WineBianhao='" + WineBianhao + '\'' +
+                ", WineCount=" + WineCount +
+                ", WineContain='" + WineContain + '\'' +
+                ", WineImg1='" + WineImg1 + '\'' +
+                ", WineImg2='" + WineImg2 + '\'' +
+                ", WineImg3='" + WineImg3 + '\'' +
+                ", WineImg4='" + WineImg4 + '\'' +
+                ", WineImg5='" + WineImg5 + '\'' +
+                ", WineImg6='" + WineImg6 + '\'' +
+                ", deleteId=" + deleteId +
+                '}';
     }
 
     public Integer getWId() {
@@ -50,6 +87,22 @@ public class Wine {
 
     public void setWineLiter(String wineLiter) {
         WineLiter = wineLiter;
+    }
+
+    public String getWineEvalution() {
+        return WineEvalution;
+    }
+
+    public void setWineEvalution(String wineEvalution) {
+        WineEvalution = wineEvalution;
+    }
+
+    public Integer getWineEvaCount() {
+        return WineEvaCount;
+    }
+
+    public void setWineEvaCount(Integer wineEvaCount) {
+        WineEvaCount = wineEvaCount;
     }
 
     public double getWinePrice() {
@@ -116,12 +169,44 @@ public class Wine {
         WineShiyong = wineShiyong;
     }
 
+    public String getWineCity() {
+        return WineCity;
+    }
+
+    public void setWineCity(String wineCity) {
+        WineCity = wineCity;
+    }
+
+    public String getFromAdress() {
+        return FromAdress;
+    }
+
+    public void setFromAdress(String fromAdress) {
+        FromAdress = fromAdress;
+    }
+
+    public String getWineBianhao() {
+        return WineBianhao;
+    }
+
+    public void setWineBianhao(String wineBianhao) {
+        WineBianhao = wineBianhao;
+    }
+
     public Integer getWineCount() {
         return WineCount;
     }
 
     public void setWineCount(Integer wineCount) {
         WineCount = wineCount;
+    }
+
+    public String getWineContain() {
+        return WineContain;
+    }
+
+    public void setWineContain(String wineContain) {
+        WineContain = wineContain;
     }
 
     public String getWineImg1() {
@@ -180,35 +265,12 @@ public class Wine {
         this.deleteId = deleteId;
     }
 
-    @Override
-    public String toString() {
-        return "Wine{" +
-                "WId=" + WId +
-                ", WineName='" + WineName + '\'' +
-                ", WineLiter='" + WineLiter + '\'' +
-                ", WinePrice=" + WinePrice +
-                ", WineCompany='" + WineCompany + '\'' +
-                ", WineSales='" + WineSales + '\'' +
-                ", WineScent='" + WineScent + '\'' +
-                ", WineBrand='" + WineBrand + '\'' +
-                ", WineDegree='" + WineDegree + '\'' +
-                ", WineAdress='" + WineAdress + '\'' +
-                ", WineShiyong='" + WineShiyong + '\'' +
-                ", WineCount=" + WineCount +
-                ", WineImg1='" + WineImg1 + '\'' +
-                ", WineImg2='" + WineImg2 + '\'' +
-                ", WineImg3='" + WineImg3 + '\'' +
-                ", WineImg4='" + WineImg4 + '\'' +
-                ", WineImg5='" + WineImg5 + '\'' +
-                ", WineImg6='" + WineImg6 + '\'' +
-                ", deleteId=" + deleteId +
-                '}';
-    }
-
-    public Wine(Integer WId, String wineName, String wineLiter, double winePrice, String wineCompany, String wineSales, String wineScent, String wineBrand, String wineDegree, String wineAdress, String wineShiyong, Integer wineCount, String wineImg1, String wineImg2, String wineImg3, String wineImg4, String wineImg5, String wineImg6, Integer deleteId) {
+    public Wine(Integer WId, String wineName, String wineLiter, String wineEvalution, Integer wineEvaCount, double winePrice, String wineCompany, String wineSales, String wineScent, String wineBrand, String wineDegree, String wineAdress, String wineShiyong, String wineCity, String fromAdress, String wineBianhao, Integer wineCount, String wineContain, String wineImg1, String wineImg2, String wineImg3, String wineImg4, String wineImg5, String wineImg6, Integer deleteId) {
         this.WId = WId;
         WineName = wineName;
         WineLiter = wineLiter;
+        WineEvalution = wineEvalution;
+        WineEvaCount = wineEvaCount;
         WinePrice = winePrice;
         WineCompany = wineCompany;
         WineSales = wineSales;
@@ -217,7 +279,11 @@ public class Wine {
         WineDegree = wineDegree;
         WineAdress = wineAdress;
         WineShiyong = wineShiyong;
+        WineCity = wineCity;
+        FromAdress = fromAdress;
+        WineBianhao = wineBianhao;
         WineCount = wineCount;
+        WineContain = wineContain;
         WineImg1 = wineImg1;
         WineImg2 = wineImg2;
         WineImg3 = wineImg3;
