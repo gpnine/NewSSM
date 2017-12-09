9d0071e393f61e1620bbbc4b9d2e104dec436325
liujunjun137324 <31762511+liujunjun137324@users.noreply.github.com>
2017-12-09 20:06:19 +0800

    orders
    orders

--- src/main/java/com/lanou/controller/OrdersController.java
+++ src/main/java/com/lanou/controller/OrdersController.java
@@ -2,6 +2,7 @@ package com.lanou.controller;
 
 import com.lanou.entity.Orders;
 import com.lanou.service.OrdersService;
+import com.lanou.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,18 +16,24 @@ public class OrdersController {
 
     @Resource
     private OrdersService ordersService;
+    @Resource
+    private UserService userService;
 
     @RequestMapping("/findOrders.do")
     @ResponseBody
     public List<Orders> findOrders() {
-         List<Orders> orders = ordersService.findOrders();
+        List<Orders> orders = ordersService.findOrders();
+        int userId = orders.get(0).getUserId();
+        System.out.println(userId);
+        userService.findUserById(userId);
         return orders;
     }
+
     @RequestMapping("/insertOrders.do")
     @ResponseBody
     public int insertOrders(String OrderPay, String OrderWuliu, double OrderAllMoney, int OrderWineId, int UserId, int WineId, String OrderTicket, String OrderText, int OrderScore, double OrderYunfei) {
-        System.out.printf("OrderYunfei:"+OrderYunfei);
-        int result = ordersService.insertOrders(OrderPay,OrderWuliu,OrderAllMoney,OrderWineId,UserId,WineId,OrderTicket,OrderText,OrderScore,OrderYunfei);
+        System.out.printf("OrderYunfei:" + OrderYunfei);
+        int result = ordersService.insertOrders(OrderPay, OrderWuliu, OrderAllMoney, OrderWineId, UserId, WineId, OrderTicket, OrderText, OrderScore, OrderYunfei);
         return result;
     }
 
